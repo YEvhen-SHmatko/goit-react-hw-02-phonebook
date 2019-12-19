111b2c6069b2c294792ef5dc632853b6d97bdef7
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2019-12-19 20:08:40 +0200

    edit export

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,31 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Styles from './ContactList.module.css';
 
-export default function ContactList({ filterContacts, onClickDelete }) {
-  return (
-    <>
-      <ul className={Styles.list}>
-        {filterContacts.map(e => {
-          return (
-            <li key={e.id} className={Styles.item}>
-              <span className={Styles['item-name']}>{e.name}:</span>
-              <span className={Styles['item-number']}>{e.number}</span>
-              <button
-                className={Styles.button}
-                type="button"
-                onClick={onClickDelete}
-                id={e.id}
-              >
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </>
-  );
-}
+const ContactList = ({ filterContacts, onClickDelete }) => (
+  <>
+    <ul className={Styles.list}>
+      {filterContacts.map(e => {
+        return (
+          <li key={e.id} className={Styles.item}>
+            <span className={Styles['item-name']}>{e.name}:</span>
+            <span className={Styles['item-number']}>{e.number}</span>
+            <button
+              className={Styles.button}
+              type="button"
+              onClick={onClickDelete}
+              id={e.id}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  </>
+);
 ContactList.propTypes = {
   filterContacts: PropTypes.arrayOf(PropTypes.object).isRequired,
   onClickDelete: PropTypes.func.isRequired,
 };
+export default ContactList;
